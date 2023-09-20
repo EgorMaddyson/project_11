05541d4adadca876c68d46c0cd4424ae185baf6b
EgorMaddyson <143204498+EgorMaddyson@users.noreply.github.com>
2023-09-21 03:30:59 +0700

    Update data.py

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ orders_body = {
     "metroStation": 9,
     "phone": "+7 800 555 35 35",
     "rentTime": 5,
-    "deliveryDate": "2023-19-09",
+    "deliveryDate": "2023-09-09",
     "comment": "Where is Naruto?",
     "color": []
 }
